00027bc94d4b9087b1957452120f15589b9ec3ca
Prosper Ihionu <echezonaprosper100@gmail.com>
2024-01-15 13:51:13 -0800

    MyPy commit

---
+++ newclass.py
@@ -0,0 +1,52 @@
+name = "Prosper"
+house = "hut"
+people = "family"
+place = "village"
+print(f"There was once a man named {name} who lived in a  {house} with his {people} in his {place}. He liked going to the farm with his {people}")
+
+a = 1
+b = 2
+nameCount = "john "
+print(nameCount * b)
+
+c = 4
+c *=2
+print(c)
+
+firstName = "john"
+print(firstName)
+
+print(firstName.replace("j", "k"))
+
+num1 = 4
+num2 = 2
+print(num1 % num2)
+
+b = 3
+c = 2
+print(b > c)
+
+d = 3
+e = 3
+print(d == e)
+
+b = 3
+c = 3
+print(b >= c)
+
+b = 1
+c = 2
+print(b >= c)
+
+b = 2
+c = 2
+print(b != c)
+
+n = 5
+k = 3
+t = 3
+f = 7
+
+print( k < n and f > t)
+print(k == t or f < n)
+print( f == k and not k >t)
